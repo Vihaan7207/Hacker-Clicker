ecbb9d574893bf5fb31457f3c017af0bf8ce4f43
Vihaan Mikkilineni <vmikki7207@gmail.com>
2022-12-18 09:56:17 -0500

    commiting autonomously

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
-git add index.html .gitignore button.jpg compile.sh git.sh script.js style.css style.scss package.json
+git add .
 git commit -m "commiting autonomously"
 git push --set-upstream origin main
